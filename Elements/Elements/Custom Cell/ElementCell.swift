f8eaf861cc19d4d0022782ceacc272fe482e52bc
maitreebain <maitreebain@gmail.com>
2019-12-19 10:25:16 -0500

    made files, working on UI

---
+++ Elements/Elements/Custom Cell/ElementCell.swift
@@ -0,0 +1,14 @@
+//
+//  ElementCell.swift
+//  Elements
+//
+//  Created by Maitree Bain on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ElementCell: UITableViewCell {
+
+    
+}
